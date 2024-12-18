76bd59b681003a8030c6301b54a32728581f464b
greg davis <djgrego@djgrego.com>
2024-12-17 23:16:25 -0600

    Plays stream.

---
+++ Audionix/wwwroot/js/audioControl.js
@@ -0,0 +1,13 @@
+﻿window.playAudio = () => {
+    const audioPlayer = document.getElementById('audioPlayer');
+    if (audioPlayer) {
+        audioPlayer.play();
+    }
+};
+
+window.pauseAudio = () => {
+    const audioPlayer = document.getElementById('audioPlayer');
+    if (audioPlayer) {
+        audioPlayer.pause();
+    }
+};
