103ffa12ebf9712e11ca1944c1fb3e682571516a
greg davis <djgrego@djgrego.com>
2025-01-05 23:04:50 -0600

    Prepare new music scheduler

---
+++ SharedLibrary/Models/MusicSchedule/SongSchedule.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedLibrary.Models.MusicSchedule
+{
+    public class SongSchedule
+    {
+        public int Id { get; set; }
+        public int AudioMetadataId { get; set; }
+        public AudioMetadata? AudioMetadata { get; set; }
+        public Category SongCategory { get; set; }
+        public Tempo SongTempo { get; set; }
+        public EnergyLevel SongEnergyLevel { get; set; }
+        public int PlayCount { get; set; }
+        public int LastPlayed { get; set; }
+
+        public enum Category
+        {
+            Current,
+            Recurrent,
+            Gold,
+            Power,
+            Specialty,
+            Holiday,
+            Custom,
+            None
+        }
+        public enum Tempo
+        {
+            Slow,
+            Medium,
+            Fast
+        }
+        public enum EnergyLevel
+        {
+            Low,
+            Medium,
+            High
+        }
+
+    }
+}
