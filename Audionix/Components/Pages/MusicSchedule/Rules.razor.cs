011435a27b61a7b0a6c8dd256c0cded67cd243a5
greg davis <djgrego@djgrego.com>
2025-01-16 17:11:29 -0600

    Udated Rules.razor.cs

--- Audionix/Components/Pages/MusicSchedule/Rules.razor.cs
+++ Audionix/Components/Pages/MusicSchedule/Rules.razor.cs
@@ -8,6 +8,26 @@ namespace Audionix.Components.Pages.MusicSchedule
 {
     public partial class Rules
     {
+        private string? newCategory;
+        private string? newSoundCode;
+        private string? newTempo;
+        private string? newEnergyLevel;
+        private string? newDaypart;
+        private string? newLevel;
+        private string? newSoundwave;
+        private SongSchedule songSchedule = new();
 
+        public void AddCategory()
+        {
+
+        }
+        public void RemoveCategory(string? category) { }
+        public void AddSoundCode() { }
+        public void RemoveSoundCode(char soundCode) { }
+        public void NewEvergyLevel(SongSchedule songSchedule) { }
+        public void RemoveEnergyLevel(string? energyLevel) { }
+        public void AddEnergyLevelk(SongSchedule songSchedule) { }
+        public void OnRowEnter(SongSchedule songSchedule) { }
+        public void AddEnergyLevel(SongSchedule songSchedule) { }
     }
 }
