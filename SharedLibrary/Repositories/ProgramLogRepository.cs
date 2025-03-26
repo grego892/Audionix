d29ed8531d4aee99abe2df61ac626fd38e685fc1
greg davis <djgrego@djgrego.com>
2025-03-26 07:52:43 -0500

    Corrected Studio log organise by station id

--- SharedLibrary/Repositories/ProgramLogRepository.cs
+++ SharedLibrary/Repositories/ProgramLogRepository.cs
@@ -166,12 +166,17 @@ namespace SharedLibrary.Repositories
                     .OrderBy(log => log.LogOrderID)
                     .ToListAsync();
 
+                // Remove the items to shift
+                context.Log.RemoveRange(itemsToShift);
+                await context.SaveChangesAsync();
+
+                // Update LogOrderID and re-add the items
                 foreach (var item in itemsToShift)
                 {
                     item.LogOrderID++;
                 }
 
-                context.Log.UpdateRange(itemsToShift);
+                await context.Log.AddRangeAsync(itemsToShift);
                 await context.SaveChangesAsync();
             }
             catch (Exception ex)
@@ -180,6 +185,32 @@ namespace SharedLibrary.Repositories
                 throw;
             }
         }
+
+
+        //public async Task ShiftLogItemsDownAsync(Guid stationId, int startIndex)
+        //{
+        //    try
+        //    {
+        //        using var context = _dbContextFactory.CreateDbContext();
+        //        var itemsToShift = await context.Log
+        //            .Where(log => log.StationId == stationId && log.LogOrderID >= startIndex)
+        //            .OrderBy(log => log.LogOrderID)
+        //            .ToListAsync();
+
+        //        foreach (var item in itemsToShift)
+        //        {
+        //            item.LogOrderID++;
+        //        }
+
+        //        context.Log.UpdateRange(itemsToShift);
+        //        await context.SaveChangesAsync();
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Log.Error(ex, "Error in ShiftLogItemsDownAsync");
+        //        throw;
+        //    }
+        //}
 
         public async Task ShiftLogItemsUpAsync(Guid stationId, int startIndex)
         {
