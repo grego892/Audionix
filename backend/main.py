6f600ae5ccc3264a418168f4bede8992fc5d8978
gregdavis <djgrego@djgrego.com>
2025-06-29 17:07:36 -0500

    cors 3

--- backend/main.py
+++ backend/main.py
@@ -54,9 +54,12 @@ security = HTTPBearer(
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 # Allow frontend origin
+from fastapi.middleware.cors import CORSMiddleware
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["https://audionix.djgrego.com", "http://localhost", "http://127.0.0.1", "http://localhost:8081"],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
